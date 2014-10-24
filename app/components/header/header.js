a47d7bf6c6c4f744eb06f7ee40de6e9b7142e5a2
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-10-24 07:38:16 +0700

    Topbar active item

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -3,13 +3,20 @@
 angular.module('header', ['header.controllers']);
 angular.module('header.controllers', [])
     .controller('HeaderCtrl', [
-	'$rootScope', '$scope', 'AuthService',
-	function($rootScope, $scope, AuthService) {
+	'$rootScope', '$scope', '$location', 'AuthService',
+	function($rootScope, $scope, $location, AuthService) {
+	    $scope.getActiveItem = function(path) {
+		if ($location.path().substr(0, path.length) == path) {
+		    return "active";
+		} else {
+		    return "";
+		}
+	    };
 	    $scope.logout = function() {
 		AuthService.logout();
 	    };
 	    $scope.menus = [
-		{'title': 'Trang chủ', 'link': '/home'},
+		{'title': 'Trang chủ', 'link': '/'},
 		{'title': 'Hoạt động', 'link': '/activity'},
 		{'title': 'Thảo luận', 'link': '/discussion'}
 	    ];
