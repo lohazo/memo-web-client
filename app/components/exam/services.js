f1bd77eb24e6502b5f7ed6de64a62bf634607c33
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-11-01 15:11:11 +0700

    Exam logic

--- app/components/exam/services.js
+++ app/components/exam/services.js
@@ -2,35 +2,86 @@
 
 angular.module('exam.services', [])
     .factory('Exam', ['ExamServices', 'Question', function(ExamServices, Question) {
-	var questions;
+	var exam, questions, answered, wrongAnswers, question, questionPosition,
+	    hearts, availableItems, examToken;
+
 	function start(data) {
 	    return ExamServices.start(data).then(function(response) {
-		questions = response.data;
+		init(response.data);
 	    });
+	}
+
+	function init(data) {
+	    // questions = data.questions.filter(function(q) {return q.type === 'form';});
+	    questions = data.questions;
+	    hearts = {
+		remaining: data.max_hearts_count,
+		lost: 0
+	    };
+	    availableItems = data.available_item;
+	    examToken = data.exam_token;
+	    answered = 0;
+	    questionPosition = 0;
+	    question = questions[questionPosition];
 	}
 
 	function getQuestions() {
 	    return questions;
 	}
 
-	function getCurrentQuestion() {
+	function getQuestion() {
+	    return question;
 	}
 
-	function getNextQuestion() {
+	function getQuestionPosition() {
+	    return questionPosition;
+	}
+
+	function getWrongAnswers() {
+	    return wrongAnswers;
+	}
+
+	function getAnswered() {
+	    return answered;
+	}
+
+	function getHearts() {
+	    return hearts;
+	}
+
+	function check(isCorrect) {
+	    answered += 1;
+	}
+
+	function next() {
+	    questionPosition += 1;
+	    question = questions[questionPosition];
 	}
 
 	function skip() {
+	    hearts.remaining = hearts.remaining - 1;
+	    hearts.lost += 1;
+	    answered += 1;
+	    next();
 	}
 
-	function finish() {
+	function finish(data) {
+	    return ExamServices.finish(data).then(function(response) {
+	    });
 	}
 
 	return {
 	    start: start,
 	    skip: skip,
 	    finish: finish,
-	    currentQuestion: getCurrentQuestion,
-	    nextQuestion: getNextQuestion
+	    next: next,
+	    check: check,
+	    answered: getAnswered,
+	    wrongAnswers: getWrongAnswers,
+	    questions: getQuestions,
+	    question: getQuestion,
+	    questionPosition: getQuestionPosition,
+	    hearts: getHearts
 	};
     }])
     .factory('ExamServices', [
@@ -49,7 +100,8 @@ angular.module('exam.services', [])
 		    type: 'lesson',
 		    auth_token: user.auth_token,
 		    lesson_number: data.lesson_number,
-		    skill_id: data.skill_id
+		    skill_id: data.skill_id,
+		    device: 'web'
 		};
 
 		$http.post(BASE_URL + '/exam/start', requestData)
@@ -57,10 +109,24 @@ angular.module('exam.services', [])
 			deferred.resolve(response);
 		    });
 
+		$http.get('/assets/data/exam_1.json')
+		    .then(function(response) {
+			deferred.resolve(response);
+		    });
+
+		return deferred.promise;
+	    }
+
+	    function finish(data) {
+		var deferred = $q.defer();
+
+
+
 		return deferred.promise;
 	    }
 
 	    return {
-		start: start
+		start: start,
+		finish: finish
 	    };
 	}]);
--- app/exam/controllers.js
+++ app/exam/controllers.js
@@ -2,13 +2,92 @@
 
 angular.module('exam.controllers', [])
     .controller('ExamCtrl', [
-	'$scope', '$routeParams', 'Exam',
-	function($scope, $routeParams, Exam) {
+	'$scope', '$timeout', '$routeParams', '$location', 'Exam', 'Question',
+	function($scope, $timeout, $routeParams, $location, Exam, Question) {
 	    var requestData = {
 		skill_id: $routeParams.id,
 		lesson_number: $routeParams.lesson_number
 	    };
 
-	    Exam.start(requestData);
+	    $scope.questionTpl = '';
+	    $scope.footerTpl = 'footer';
+
+	    var questionTplId = {
+		form: 'questionForm',
+		judge: 'questionJudge',
+		listen: 'questionListen',
+		name: 'questionName',
+		select: 'questionSelect',
+		speak: 'questionSpeak',
+		translate: 'questionTranslate',
+		failure: 'questionFailure',
+		success: 'questionSuccess'
+	    };
+
+	    var footerTplId = {
+		footer: 'footer',
+		failure: 'footerFailure',
+		success: 'footerSuccess',
+		result: 'footerResult'
+	    };
+
+	    $scope.question = { };
+	    $scope.result = { };
+	    $scope.hearts = {
+		remaining: 0,
+		lost: 0
+	    };
+	    $scope.userAnswer = '';
+
+	    $scope.quit = function() {
+		delete $scope.exam;
+		$location.path('/');
+	    };
+
+	    $scope.skip = function() {
+		$scope.result = Question.skip($scope.question, '');
+		Exam.skip();
+		$scope.hearts = Exam.hearts();
+		$scope.footerTpl = "footerResult";
+	    };
+
+	    $scope.check = function() {
+		if ($scope.question.userAnswer && $scope.question.userAnswer.length > 0) {
+		    $scope.result = Question.check($scope.question, $scope.question.userAnswer);
+		    $scope.footerTpl = "footerResult";
+		    if (!$scope.result.result) {
+			Exam.skip();
+			$scope.hearts = Exam.hearts();
+		    } else {
+			Exam.check();
+		    }
+		    $scope.answered = Exam.answered();
+		}
+	    };
+
+	    $scope.nextQuestion = function() {
+		$scope.questionTpl = "";
+		$scope.footerTpl = "footer";
+
+		// Aggressively update
+		$timeout(function() {
+		    Exam.next();
+		    $scope.question = Exam.question();
+		    $scope.answered = Exam.answered();
+		    $scope.ant = Exam.questionPosition();
+		    $scope.question.userAnswer = "";
+		    $scope.questionTpl = questionTplId[$scope.question.type];
+		}, 200);
+	    };
+
+	    Exam.start(requestData).then(function(response) {
+		$scope.questions = Exam.questions();
+		$scope.question = Exam.question();
+		$scope.answered = Exam.answered();
+		$scope.ant = 0;
+		$scope.hearts = Exam.hearts();
+		$scope.question.userAnswer = "";
+		$scope.questionTpl = questionTplId[$scope.question.type];
+	    });
 	}
     ]);
