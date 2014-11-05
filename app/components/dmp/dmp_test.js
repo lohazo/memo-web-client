267c98d2df199d1d44156ef3af1f57fedabb0035
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-11-05 21:15:37 +0700

    port google dmp and tests

---
+++ app/components/dmp/dmp_test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+describe('Service', function() {
+    // load modules
+    beforeEach(module('diff-match-patch'));
+
+    describe('dmp', function() {
+	var Dmp, scope;
+
+	beforeEach(inject(function($rootScope, dmp) {
+	    scope = $rootScope.$new();
+	    Dmp = dmp;
+	}));
+
+	it('check if dmp exists', function() {
+	    expect(Dmp).toBeDefined();
+	});
+
+	it('check if diff_main exists', function() {
+	    expect(Dmp.diff_main).toBeDefined();
+	});
+	
+	it('should have default values', function() {
+	    expect(Dmp.DIFF_INSERT).toEqual(1);
+	    expect(Dmp.DIFF_EQUAL).toEqual(0);
+	    expect(Dmp.DIFF_DELETE).toEqual(-1);
+	});
+    });
+});
