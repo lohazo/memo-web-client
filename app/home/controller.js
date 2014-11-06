c6796d9440acbe45bd786c87325cf97b7c29720a
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-11-07 05:58:51 +0700

    Get app settings

--- app/home/controller.js
+++ app/home/controller.js
@@ -1,38 +1,40 @@
 'use strict';
 
-angular.module('home.controller', [])
+angular.module('home.controller', ['app.services'])
     .controller('HomeCtrl', ['$scope', function($scope) {
     }])
     .controller('HomeMainCtrl', [
 	'$scope', 'Profile', 'Course', 'TreeBuilder',
-	function($scope, Profile, Course, TreeBuilder) {
+	'AppSetting',
+	function($scope, Profile, Course, TreeBuilder, AppSetting) {
 	    Profile.getProfile($scope.auth.user)
 		.then(function() {
 		    $scope.profile = Profile.getData().user_info;
 		})
+		.then(AppSetting.get)
 		.then(function() {
 		    TreeBuilder.getCheckpoints();
 		    TreeBuilder.getSkills();
 		    TreeBuilder.getTree();
 		    $scope.skillTree = TreeBuilder.build();
-		});
-
-	    Profile.getProfileDetail($scope.auth.user)
-		.then(function() {
-		    $scope.profileDetail = Profile.getDetail();
-		    $scope.expChart = {
-			labels: $scope.profileDetail.exp_chart.days,
-			datasets: [{
-			    label: "",
-			    fillColor : "rgba(220,220,220,0.2)",
-			    strokeColor : "#848484",
-			    pointColor : "#810c15",
-			    pointStrokeColor : "#fff",
-			    pointHighlightFill : "#fff",
-			    pointHighlightStroke : "rgba(220,220,220,1)",
-			    data : $scope.profileDetail.exp_chart.exp
-			}]
-		    };
+		}).then(function() {
+		    Profile.getProfileDetail($scope.auth.user)
+			.then(function() {
+			    $scope.profileDetail = Profile.getDetail();
+			    $scope.expChart = {
+				labels: $scope.profileDetail.exp_chart.days,
+				datasets: [{
+				    label: "",
+				    fillColor : "rgba(220,220,220,0.2)",
+				    strokeColor : "#848484",
+				    pointColor : "#810c15",
+				    pointStrokeColor : "#fff",
+				    pointHighlightFill : "#fff",
+				    pointHighlightStroke : "rgba(220,220,220,1)",
+				    data : $scope.profileDetail.exp_chart.exp
+				}]
+			    };
+			});
 		});
 	}
     ])
