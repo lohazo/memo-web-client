df079b5f354cec24b92c92c7736925530a37e1fe
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-11-10 07:45:34 +0700

    Profile setting

---
+++ app/settings/profile.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('settings.profile', [])
+    .controller('SettingProfileCtrl', ['$scope', function($scope) {
+	$scope.settingProfile = {
+	};
+    }]);
--- app/settings/settings.js
+++ app/settings/settings.js
@@ -3,6 +3,8 @@
 angular.module('settings', [
     'settings.sidebar',
     'settings.account',
+    'settings.profile',
+    'settings.languages',
     'settings.notification'
 ]).config(['$routeProvider', '$locationProvider', SettingConfig]);
 
@@ -10,6 +12,14 @@ function SettingConfig($routeProvider, $locationProvider) {
     $routeProvider.when('/setting/account', {
 	templateUrl: 'settings/_account.html',
 	controller: 'SettingAccountCtrl'
+    });
+    $routeProvider.when('/setting/profile', {
+	templateUrl: 'settings/_profile.html',
+	controller: 'SettingProfileCtrl'
+    });
+    $routeProvider.when('/setting/languages', {
+	templateUrl: 'settings/_languages.html',
+	controller: 'SettingLanguageCtrl'
     });
     $routeProvider.when('/setting/notification', {
 	templateUrl: 'settings/_notification.html',
