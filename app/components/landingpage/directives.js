a2a6ce1f133ec43428b4156d59104b226e0d29cd
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-11-17 09:20:31 +0700

    Update landingpage

--- app/components/landingpage/directives.js
+++ app/components/landingpage/directives.js
@@ -40,45 +40,31 @@ angular.module('landingpage.directives', [])
             controller: 'LpStatCtrl',
 	    link: function($scope, $element) {
 		$scope.users = {
-		ios: 0,
-		android: 0,
-		wp: 0,
-		pc: 0,
-		total: 0
-	    };
-	    var upperBound = {
-		ios: 232,
-		android: 408,
-		wp: 72,
-		pc: 312
-	    };
-	    
-	    upperBound.total = upperBound.ios + upperBound.android + upperBound.wp + upperBound.pc;
-
-	    $interval(function() {
-		$scope.users.total += 1;
-		// localStorage.users = angular.toJson($scope.users);
-	    }, 2000 / upperBound.total, upperBound.total);
-	    
-	    $interval(function() {
-		$scope.users.ios += 1;
-		// localStorage.users = angular.toJson($scope.users);
-	    }, 2000 / upperBound.ios, upperBound.ios);
-	    
-	    $interval(function() {
-		$scope.users.android += 1;
-		// localStorage.users = angular.toJson($scope.users);
-	    }, 2000 / upperBound.android, upperBound.android);
-	    
-	    $interval(function() {
-		$scope.users.wp += 1;
-		// localStorage.users = angular.toJson($scope.users);
-	    }, 2000 / upperBound.wp, upperBound.wp);
-	    
-	    $interval(function() {
-		$scope.users.pc += 1;
-		// localStorage.users = angular.toJson($scope.users);
-	    }, 2000 / upperBound.pc, upperBound.pc);
+		    ios: 232,
+		    android: 408,
+		    wp: 0,
+		    pc: 312
+		};
+		
+		$scope.users.total = $scope.users.ios + $scope.users.android + $scope.users.pc;
+		
+		$interval(function() {
+		    $scope.users.ios += 1;
+		    $scope.users.total += 1;
+		    // localStorage.users = angular.toJson($scope.users);
+		}, Math.floor(Math.random() * 10 + 10) * 1000);
+		
+		$interval(function() {
+		    $scope.users.android += 1;
+		    $scope.users.total += 1;
+		    // localStorage.users = angular.toJson($scope.users);
+		}, Math.floor(Math.random() * 10 + 10) * 1000);
+		
+		$interval(function() {
+		    $scope.users.pc += 1;
+		    $scope.users.total += 1;
+		    // localStorage.users = angular.toJson($scope.users);
+		}, Math.floor(Math.random() * 10 + 10) * 1000);
 	    },
             templateUrl: 'components/landingpage/_statistic.html'
         };
