c570f27e419dc80fe3510a2298fa78037565a08d
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-11-28 18:50:46 +0700

    Track lessons list

--- app/skill/controllers.js
+++ app/skill/controllers.js
@@ -2,32 +2,12 @@
 
 angular.module('skill.controllers', [])
     .controller('SkillCtrl', [
-	'$scope', '$rootScope', '$location', '$localStorage', '$routeParams',
-	'AuthService', 'Skill',
-	function($scope, $rootScope, $location, $localStorage, $routeParams,
-		 AuthService, Skill) {
-	    $scope.auth = $localStorage.auth || {
-		loggedIn: false,
-		trial: false
-	    };
-	    $rootScope.$on('event:auth-invalidToken', function(e, data) {
-		AuthService.logout();
-	    });
-	    $rootScope.$on('event:auth-loginConfirmed', function(e, data) {
-		$scope.auth = {
-		    loggedIn: true,
-		    user: data.user,
-		    trial: data.is_trial
-		};
-		$localStorage.auth = $scope.auth;
-	    });
+        '$scope', '$rootScope', '$location', '$localStorage', '$routeParams',
+        'AuthService', 'Skill', 'MemoTracking',
+        function($scope, $rootScope, $location, $localStorage, $routeParams,
+            AuthService, Skill, MemoTracker) {
 
-	    $rootScope.$on('event:auth-logoutConfirmed', function(e, data) {
-		$scope.auth = { loggedIn: false, trial: false };
-		$localStorage.auth = $scope.auth;
-		$location.path('/');
-	    });
-
-	    $scope.skill = Skill.skill($routeParams.id);
-	}
+            $scope.skill = Skill.skill($routeParams.id);
+            MemoTracker.track('lessons list');
+        }
     ]);
