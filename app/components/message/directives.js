907d039bb56e0fb92127075119986ca90db873c0
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-12-05 10:43:51 +0700

    Update message panel checking condition

--- app/components/message/directives.js
+++ app/components/message/directives.js
@@ -7,7 +7,7 @@ angular.module('message.directives', [])
             controller: 'MessageListCtrl',
             link: function($scope, $element, $attr) {
                 var messages = $localStorage.messages;
-                $scope.shouldOpen = messages && messages.message_ids.length > 0;
+                $scope.shouldOpen = messages && messages.message_ids && messages.message_ids.length > 0;
                 $scope.closeMessageList = function() {
                     $scope.shouldOpen = false;
                     $scope.openMessage(messages);
