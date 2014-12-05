e46040dc4dc3e5017a4857643d0ad29c847b69b9
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-12-05 17:00:56 +0700

    App Setting

--- app/settings/languages.js
+++ app/settings/languages.js
@@ -1,7 +1,18 @@
 (function(angular) {
 
-    'use strict';
+  'use strict';
 
-    angular.module('settings.languages', []);
-    
+  function SettingLanguageService($http, $q, $localStorage) {
+    var Setting = {};
+
+    return Setting;
+  }
+
+  function SettingLanguageCtrl($scope, SettingLanguageService) {
+  }
+
+  angular.module('settings.languages', [])
+    .controller('SettingLanguageCtrl', ['$scope', 'SettingLanguageService', SettingLanguageCtrl])
+    .factory('SettingLanguageService', ['$http', '$q', '$localStorage', SettingLanguageService]);
+
 })(window.angular);
--- app/settings/settings.js
+++ app/settings/settings.js
@@ -1,31 +1,27 @@
-(function(angular) {
-    'use strict';
+(function (angular) {
+  'use strict';
 
-    angular.module('settings', [
-        'settings.sidebar',
-        'settings.account',
-        'settings.profile',
-        'settings.languages',
-        'settings.notification'
-    ]).config(['$routeProvider', '$locationProvider', SettingConfig]);
+  function SettingConfig($routeProvider) {
+    $routeProvider.when('/setting/account', {
+      templateUrl: 'settings/_account.html',
+      controller: 'SettingAccountCtrl'
+    });
+    $routeProvider.when('/setting/profile', {
+      templateUrl: 'settings/_profile.html',
+      controller: 'SettingProfileCtrl'
+    });
+    $routeProvider.when('/setting/direction', {
+      templateUrl: 'settings/_languages.html',
+      controller: 'SettingLanguageCtrl'
+    });
+    $routeProvider.when('/setting/notification', {
+      templateUrl: 'settings/_notification.html',
+      controller: 'SettingNotificationCtrl'
+    });
+  }
 
-    function SettingConfig($routeProvider, $locationProvider) {
-        $routeProvider.when('/setting/account', {
-            templateUrl: 'settings/_account.html',
-            controller: 'SettingAccountCtrl'
-        });
-        $routeProvider.when('/setting/profile', {
-            templateUrl: 'settings/_profile.html',
-            controller: 'SettingProfileCtrl'
-        });
-        $routeProvider.when('/setting/languages', {
-            templateUrl: 'settings/_languages.html',
-            controller: 'SettingLanguageCtrl'
-        });
-        $routeProvider.when('/setting/notification', {
-            templateUrl: 'settings/_notification.html',
-            controller: 'SettingNotificationCtrl'
-        });
-    }
-
-})(window.angular);
+  angular.module('settings', [
+    'settings.sidebar', 'settings.account', 'settings.profile',
+    'settings.languages', 'settings.notification'
+  ]).config(['$routeProvider', SettingConfig]);
+}(window.angular));
