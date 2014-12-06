b6e2d40a5a945388d7676a83c2e09076a5803530
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-12-06 21:27:53 +0700

    update profile

--- app/components/profile/services.js
+++ app/components/profile/services.js
@@ -1,64 +1,93 @@
-'use strict';
+(function (angular) {
+  'use strict';
 
-angular.module('profile.services', [])
-    .factory('Profile', ['ProfileServices', '$localStorage', function(ProfileServices, $localStorage) {
-	var Profile = {};
+  function ProfileServices($http, $q, $location, $localStorage) {
+    var HOST = "http://api.memo.edu.vn/api",
+      API_VERSION = "/v1.5",
+      BASE_URL = HOST + API_VERSION;
 
-	Profile.data = {};
-	Profile.detail = {};
+    var Services = {};
 
-	Profile.getProfile = function(data) {
-	    return ProfileServices.profile(data)
-		.then(function(response) {
-		    Profile.data = response.data;
-		    $localStorage.auth.user = Profile.data.user_info;
-		    $localStorage.auth.skills_tree = Profile.data.skills_tree;
-		    $localStorage.auth.checkpoints = Profile.data.checkpoints;
-		    $localStorage.auth.skills = Profile.data.skills;
-		});
-	};
+    Services.profile = function (data) {
+      var deferred = $q.defer();
 
-	Profile.getProfileDetail = function(data) {
-	    return ProfileServices.profileDetail(data)
-		.then(function(response) {
-		    Profile.detail = response.data;
-		    $localStorage.auth.profile_detail = Profile.detail;
-		});
-	};
+      // data = {_id: 120, auth_token: asoentuhasetu};
+      $http.get(BASE_URL + '/users/' + data._id + '?auth_token=' + data.auth_token)
+        .then(function (response) {
+          deferred.resolve(response);
+        }, function (response) {
+          if (response.status === 400) {
+            $location.path('/course');
+          }
+          deferred.reject(response);
+        });
 
-	return Profile;
-    }])
-    .factory('ProfileServices', ['$http', '$q', '$location', function($http, $q, $location) {
-	var HOST = "http://api.memo.edu.vn/api",
-	    API_VERSION = "/v1.5",
-	    BASE_URL = HOST + API_VERSION;
+      return deferred.promise;
 
-	return {
-	    profile: function(data) {
-		var deferred = $q.defer();
+    };
 
-		$http.get(BASE_URL + '/users/' + data._id + '?auth_token=' + data.auth_token)
-		    .then(function(response) {
-			deferred.resolve(response);
-		    }, function(response) {
-			if (response.status == 400) {
-			    $location.path('/course');
-			}
-			deferred.reject(response);
-		    });
+    Services.profileDetail = function (data) {
+      var deferred = $q.defer();
 
-		return deferred.promise;
-	    },
-	    profileDetail: function(data) {
-		var deferred = $q.defer();
+      $http.get(BASE_URL + '/users/profile_details' + '?device=web&auth_token=' + data.auth_token)
+        .then(function (response) {
+          deferred.resolve(response);
+        });
 
-		$http.get(BASE_URL + '/users/profile_details'
-			  + '?device=web&auth_token=' + data.auth_token)
-		    .then(function(response) {
-			deferred.resolve(response);
-		    });
+      return deferred.promise;
+    };
 
-		return deferred.promise;
-	    }
-	};
-    }]);
+    Services.update = function (data) {
+      var deferred = $q.defer();
+
+      // data = {username: 'asonetuh'/ password: 'anoethuasto'/ email: 'asoentuh'}
+      data.auth_token = $localStorage.auth.user.auth_token;
+
+      $http.post(BASE_URL + '/users/edit', data)
+        .then(function (response) {
+          deferred.resolve(response);
+        });
+
+      return deferred.promise;
+    };
+
+    return Services;
+  }
+
+  function ProfileFactory(ProfileServices, $localStorage) {
+    var Profile = {};
+
+    Profile.data = {};
+    Profile.detail = {};
+
+    Profile.getProfile = function (data) {
+      return ProfileServices.profile(data)
+        .then(function (response) {
+          Profile.data = response.data;
+          $localStorage.auth.user = Profile.data.user_info;
+          $localStorage.auth.skills_tree = Profile.data.skills_tree;
+          $localStorage.auth.checkpoints = Profile.data.checkpoints;
+          $localStorage.auth.skills = Profile.data.skills;
+        });
+    };
+
+    Profile.getProfileDetail = function (data) {
+      return ProfileServices.profileDetail(data)
+        .then(function (response) {
+          Profile.detail = response.data;
+          $localStorage.auth.profile_detail = Profile.detail;
+        });
+    };
+
+    Profile.update = function (data) {
+      // data = {email/password/username}
+      return ProfileServices.update(data);
+    };
+
+    return Profile;
+  }
+
+  angular.module('profile.services', [])
+    .factory('ProfileServices', ['$http', '$q', '$location', '$localStorage', ProfileServices])
+    .factory('Profile', ['ProfileServices', '$localStorage', ProfileFactory]);
+}(window.angular));
