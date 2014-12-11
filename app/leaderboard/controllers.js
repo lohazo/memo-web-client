480099affc8cc4536fbf34a87e95a406c0a1aad6
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-12-11 15:32:21 +0700

    Search FB friends

--- app/leaderboard/controllers.js
+++ app/leaderboard/controllers.js
@@ -3,64 +3,6 @@
   'use strict';
 
   function LeaderboardCtrl($scope, $localStorage, Leaderboard) {
-    $scope.search = {
-      name: ''
-    };
-    $scope.showTheLeaderboard = function () {
-      $scope.showLeaderboard = true;
-      $scope.showFbFriends = false;
-      $scope.showFriends = false;
-    };
-    $scope.searchFbFriends = function () {
-      Leaderboard.fbFriends().then(function (response) {
-        $scope.fbFriends = response.data.data;
-      });
-      $scope.showLeaderboard = false;
-      $scope.showFbFriends = true;
-      $scope.showFriends = false;
-    };
-    $scope.searchFriends = function () {
-      $scope.showLeaderboard = false;
-      $scope.showFbFriends = false;
-      $scope.showFriends = true;
-    };
-    $scope.showTheLeaderboard();
-  }
-
-  function LeaderboardHomeCtrl($scope, $rootScope) {
-    $rootScope.$on('event-profileLoaded', function(e, data) {
-      $scope.tabs = [{
-        'title': "Tuần này",
-        'users': data.followings_leaderboard_by_week,
-        'active': true
-      }, {
-        'title': "Tháng này",
-        'users': data.followings_leaderboard_by_month,
-        'active': false
-      }, {
-        'title': "Tổng cộng",
-        'users': data.followings_leaderboard_all_time,
-        'active': false
-      }];
-    });
-  }
-
-  function LeaderboardFbFriendsCtrl($scope) { }
-
-  function LeaderboardFriendsCtrl($scope, Leaderboard) {
-
-    $scope.search = {
-      keywords: ''
-    };
-    $scope.searchMemoFriends = function () {
-      var reqData = {
-        keywords: $scope.search.keywords,
-      };
-      Leaderboard.friends(reqData)
-        .then(function (response) {
-          $scope.friends = response.data;
-        });
-    };
     $scope.follow = function (id) {
       var reqData = {
         friend_id: id
@@ -72,6 +14,7 @@
         friend.is_following = true;
       });
     };
+
     $scope.unfollow = function (id) {
       var reqData = {
         friend_id: id
@@ -82,6 +25,73 @@
         })[0];
         friend.is_following = false;
       });
+    };
+
+    $scope.showTheLeaderboard = function () {
+      $scope.showLeaderboard = true;
+      $scope.showFbFriends = false;
+      $scope.showFriends = false;
+    };
+
+    $scope.searchFbFriends = function () {
+      Leaderboard.fbFriends().then(function (response) {
+        $scope.friends = response.data;
+      });
+      $scope.showLeaderboard = false;
+      $scope.showFbFriends = true;
+      $scope.showFriends = false;
+    };
+
+    $scope.searchFriends = function () {
+      $scope.showLeaderboard = false;
+      $scope.showFbFriends = false;
+      $scope.showFriends = true;
+    };
+
+    $scope.showTheLeaderboard();
+  }
+
+  function LeaderboardHomeCtrl($scope, $rootScope, Leaderboard) {
+    $rootScope.$on('event-profileLoaded', function(e, data) {
+      $scope.profile = data;
+    });
+    $scope.$watch('profile', function() {
+      $scope.tabs = [{
+        'title': "Tuần này",
+        'users': $scope.profile.followings_leaderboard_by_week,
+        'active': true
+      }, {
+        'title': "Tháng này",
+        'users': $scope.profile.followings_leaderboard_by_month,
+        'active': false
+      }, {
+        'title': "Tổng cộng",
+        'users': $scope.profile.followings_leaderboard_all_time,
+        'active': false
+      }];
+
+    });
+  }
+
+
+  function LeaderboardFbFriendsCtrl($scope) {
+    $scope.search = {
+      username: ''
+    };
+  }
+
+  function LeaderboardFriendsCtrl($scope, Leaderboard) {
+    $scope.search = {
+      keywords: ''
+    };
+    $scope.searchMemoFriends = function () {
+      var reqData = {
+        keywords: $scope.search.keywords,
+      };
+      Leaderboard.friends(reqData)
+        .then(function (response) {
+          $scope.friends = response.data;
+        });
     };
   }
 
