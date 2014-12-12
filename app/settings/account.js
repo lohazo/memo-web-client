6e6a462e889c4ecbca14202497f471a6092fb507
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-12-12 10:29:58 +0700

    Change account info

--- app/settings/account.js
+++ app/settings/account.js
@@ -27,18 +27,22 @@
       var data = {};
 
       if ($scope.password && $scope.rePassword && $scope.password === $scope.rePassword) {
+        data = {};
         data.password = $scope.password;
+        AccountSetting.saveChanges(data);
       }
 
       if ($scope.user.email && $scope.user.email.length > 0) {
+        data = {};
         data.email = $scope.user.email;
+        AccountSetting.saveChanges(data);
       }
 
       if ($scope.user.username && $scope.user.username.length > 0) {
+        data = {};
         data.username = $scope.user.username;
+        AccountSetting.saveChanges(data);
       }
-
-      AccountSetting.saveChanges(data);
     };
   }
 
