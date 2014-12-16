727560d87e80e32604ab4990755b83c108f941bf
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-12-17 01:27:34 +0700

    Move code to ctrl not link function

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -11,21 +11,49 @@
 
   function WelcomeCtrl($scope) {}
 
-  function PlayerCarouselCtrl($scope, AppSetting) {
+  function PlayerCarouselCtrl($scope, $location, AppSetting, MemoTraker) {
     $scope.slideIndex = 0;
     if (AppSetting.sharedSettings) {
       $scope.slides = AppSetting.sharedSettings.take_a_tour;
     } else {
-      AppSetting.getSharedSettings().then(function (response) {
+      AppSetting.getSharedSettings().then(function(response) {
         $scope.slides = AppSetting.sharedSettings.take_a_tour;
       });
     }
+
+    function next() {
+      if ($scope.slideIndex < $scope.slides.length - 1) {
+        goToSlide($scope.slideIndex + 1);
+      } else {
+        $location.path('/');
+      }
+    }
+
+    function previous() {
+      goToSlide($scope.slideIndex - 1);
+    }
+
+    function skip() {
+      $location.path('/');
+    }
+
+    function goToSlide(index) {
+      $scope.slideIndex = index;
+      $scope.slide = $scope.slides[$scope.slideIndex];
+    }
+
+    $scope.control = {
+      next: next,
+      previous: previous,
+      skip: skip,
+      goToSlide: goToSlide
+    };
   }
 
   angular.module('welcome', []).config(['$routeProvider', WelcomeConfig]);
   angular.module('welcome')
     .controller('WelcomeCtrl', ['$scope', WelcomeCtrl])
-    .controller('PlayerCarouselCtrl', ['$scope', 'AppSetting', PlayerCarouselCtrl]);
+    .controller('PlayerCarouselCtrl', ['$scope', '$location', 'AppSetting', 'MemoTracking', PlayerCarouselCtrl]);
   angular.module('welcome')
     .directive('playerContainerWelcome', function() {
       return {
@@ -49,40 +77,11 @@
       };
     });
   angular.module('welcome')
-    .directive('playerContainerCarousel', function($location) {
+    .directive('playerContainerCarousel', function() {
       return {
         strict: 'EA',
         scope: true,
         controller: 'PlayerCarouselCtrl',
-        link: function($scope, $element, $attributes) {
-          function next() {
-            if ($scope.slideIndex < $scope.slides.length - 1) {
-              goToSlide($scope.slideIndex + 1);
-            } else {
-              $location.path('/');
-            }
-          }
-
-          function previous() {
-            goToSlide($scope.slideIndex - 1);
-          }
-
-          function skip() {
-            $location.path('/');
-          }
-
-          function goToSlide(index) {
-            $scope.slideIndex = index;
-            $scope.slide = $scope.slides[$scope.slideIndex];
-          }
-
-          $scope.control = {
-            next: next,
-            previous: previous,
-            skip: skip,
-            goToSlide: goToSlide
-          };
-        },
         templateUrl: 'components/player/_player-container-carousel.html'
       };
     });
