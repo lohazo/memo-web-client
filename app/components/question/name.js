bdf90224921a209350c09561d94f2fab169e0136
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-12-19 17:44:23 +0700

    Fix question name

--- app/components/question/name.js
+++ app/components/question/name.js
@@ -1,34 +1,37 @@
-'use strict';
+(function(angular) {
+  'use strict';
 
-//Bypass jslint
-var angular = window.angular || angular;
+  function QuestionNameCtrl($scope) {
+    $scope.question = $scope.$parent.question;
+  }
 
-angular.module('question.name', [])
-    .controller('QuestionNameCtrl', [
-        '$scope',
-        '$attrs',
-        function($scope, $attrs) {
-	    $scope.question = $scope.$parent.question;
-        }])
-    .directive('questionName', function() {
-        return {
-            strict: 'EA',
-	    replace: true,
-	    scope: {
-		answer: "=answer"
-	    },
-            controller: 'QuestionNameCtrl',
-            link: function($scope, $element) {
-		$element[0].querySelector('input[type="text"]').focus();
-		$element.on('keydown', function(e) {
-		    if (e.keyCode === 13) {
-			if ($scope.answer && $scope.answer.length > 0) {
-			    e.preventDefault();
-			    $element[0].querySelector('input[type="text"]').setAttribute('readonly', 'readonly');
-			}
-		    }
-		});
-            },
-            templateUrl: 'components/question/_question-name.html'
-        };
-    });
+  function QuestionNameDirective() {
+    function directiveLink($scope, $element) {
+      $element[0].querySelector('input[type="text"]').focus();
+      $element.on('keydown', function(e) {
+        if (e.keyCode === 13) {
+          if ($scope.answer && $scope.answer.length > 0) {
+            e.preventDefault();
+            $element[0].querySelector('input[type="text"]').setAttribute('readonly', 'readonly');
+          }
+        }
+      });
+    }
+
+    return {
+      strict: 'EA',
+      replace: true,
+      scope: {
+        answer: '='
+      },
+      controller: 'QuestionNameCtrl',
+      link: directiveLink,
+      templateUrl: 'components/question/_question-name.html'
+    };
+  }
+
+  angular.module('question.name', []);
+  angular.module('question.name')
+    .controller('QuestionNameCtrl', ['$scope', QuestionNameCtrl])
+    .directive('questionName', QuestionNameDirective);
+}(window.angular));
