86c0f44d469723f0734880947f83f5a50ab4c0b5
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-12-22 10:34:41 +0700

    Refactor login services

--- app/components/login/services.js
+++ app/components/login/services.js
@@ -1,175 +1,216 @@
-'use strict';
+(function(angular) {
+  'use strict';
 
-angular.module('login.services', [])
-.factory('AuthService', [
-    '$rootScope', '$localStorage', '$routeParams',
-    'Facebook', 'GooglePlus', 'EcoTracking', 'MemoTracking',
-    'LoginService', 'MolServices',
-    function($rootScope, $localStorage, $routeParams,
-        Facebook, GooglePlus, EcoTracker, MemoTracker,
-        LoginService, MolServices) {
-        var AuthService = function() {};
-        AuthService.prototype.register = function(data) {
-            return LoginService.register(data).then(loginCallback);
-        };
+  function LoginFactory($http, $q, $localStorage, API_PHP) {
+    var Service = {};
 
-        AuthService.prototype.login = function(data) {
-            return LoginService.login(data).then(loginCallback);
-        };
-
-        AuthService.prototype.forgetPassword = function(data) {
-            return LoginService.forgetPassword(data);
-        };
-        AuthService.prototype.FbLogin = function() {
-            Facebook.getLoginStatus(function(loginStatusResponse) {
-                if (loginStatusResponse.status === 'connected') {
-                    getFbInfo(loginStatusResponse);
-                } else {
-                    Facebook.login(function(loginResponse) {
-                        if (loginResponse.status === 'unknown' || loginResponse.status === 'not_authorized') {
-
-                            $rootScope('event:auth-loginFailed', {
-                                error: "Có lỗi khi đăng nhập bằng Facebook"
-                            });
-                        } else {
-                            getFbInfo(loginResponse);
-                        }
-                    }, {
-                        scope: 'public_profile, email, user_friends'
-                    });
-                }
-            });
-        };
-        AuthService.prototype.GLogin = function() {
-            var requestData = {};
-            return GooglePlus.login().then(function(gAuthResult) {
-                requestData.g_access_token = gAuthResult.access_token;
-                GooglePlus.getUser().then(function(response) {
-                    requestData.gmail = response.result.email;
-                    LoginService.login(requestData).then(loginCallback);
-                });
-            });
-        };
-
-        AuthService.prototype.checkAuth = function() {
-            var data = $localStorage.auth.user;
-            return LoginService.checkAuth(data).then(function(response) {
-                if (response.data.response_code === 401) {
-                    $rootScope.$broadcast('event:auth-invalidToken');
-                }
-            });
-        };
-
-        AuthService.prototype.logout = function() {
-            $localStorage.$reset();
-            delete $localStorage.displayTour;
-            $rootScope.$broadcast('event:auth-logoutConfirmed');
-        };
-
-        function getFbInfo(data) {
-            var requestData = {};
-            requestData.fb_access_token = data.authResponse.accessToken;
-            Facebook.api('/me', function(user) {
-                requestData.fb_Id = user.id;
-                requestData.fb_name = user.name;
-                LoginService.login(requestData).then(loginCallback);
-            });
-        }
-
-        function loginCallback(response) {
-            $localStorage.auth = {
-                loggedIn: true,
-                user: response.data,
-                trial: response.data.is_trial
-            };
-
-            var data = angular.fromJson(angular.toJson(response.data));
-                // data.name = data.username;
-                data.name = "memo_" + data._id;
-                delete data.auth_token;
-
-            if (response.data.is_newly_sign_up) {
-                MemoTracker.track('sign up');
-                EcoTracker.track('Web 1.0.2 user logged in', data);
-                $localStorage.displayTour = true;
-            } else {
-                MemoTracker.track('login');
-            }
-            mixpanel.identify(response.data._id);
-
-                var molData = {};
-                molData.code_chanel = $routeParams.code_chanel || -100;
-                molData.id_landingpage = $routeParams.id_landingpage || -100;
-                molData.id_campaign = $routeParams.id_campaign || -100;
-                molData.id_camp_landingpage = $routeParams.id || -100;
-
-                mixpanel.track('Web 1.0.2 user logged in', molData);
-
-                molData.name = data.name || data.username;
-                molData.email = data.email;
-                molData.phone = data.mobile || '';
-
-                MolServices.saveC3(molData);
-
-                $rootScope.$broadcast('event:auth-loginConfirmed', {
-                    user: response.data
-                });
-            }
-            return new AuthService();
-        }
-        ])
-.factory('LoginService', ['$http', '$q', function($http, $q) {
-    var HOST = 'http://api.memo.edu.vn/api',
-    API_VERSION = '/v1.5',
-    BASE_URL = HOST + API_VERSION;
-
-    return {
-        register: function(data) {
-            var deferred = $q.defer();
-            $http.post(BASE_URL + '/users', data)
-            .then(function(authData) {
-                deferred.resolve(authData);
-            }, function(authData) {
-                deferred.reject(authData);
-            });
-            return deferred.promise;
-        },
-        login: function(data) {
-            var deferred = $q.defer();
-            $http.post(BASE_URL + '/users/login', data)
-            .then(function(authData) {
-                deferred.resolve(authData);
-            }, function(authData) {
-                deferred.reject(authData);
-            });
-            return deferred.promise;
-        },
-        forgetPassword: function(data) {
-            var deferred = $q.defer();
-            $http.post(BASE_URL + '/users/forget_password', data)
-            .then(function(authData) {
-                deferred.resolve(authData);
-            }, function(authData) {
-                deferred.reject(authData);
-            });
-            return deferred.promise;
-        },
-        checkAuth: function(data) {
-            var deferred = $q.defer();
-            $http.post(BASE_URL + '/users/extendauthtoken', data)
-            .then(function(authData) {
-                deferred.resolve(authData);
-            });
-            return deferred.promise;
-        },
-        profile: function(data) {
-            var deferred = $q.defer();
-            $http.get(BASE_URL + '/users/' + data._id + '?auth_token=' + data.auth_token)
-            .then(function(userProfile) {
-                deferred.resolve(userProfile);
-            });
-            return deferred.promise;
-        },
-
+    Service.register = function (data) {
+      $http.post(API_PHP + '/users', data)
+        .then(function(response) {
+          deferred.resolve(response);
+        }, function(response) {
+          deferred.reject(response);
+        });
+      return deferred.promise;
     };
-}])
+
+    Service.login = function (data) {
+      var deferred = $q.defer();
+      $http.post(API_PHP + '/users/login', data)
+        .then(function(response) {
+          deferred.resolve(response);
+        }, function(response) {
+          deferred.reject(response);
+        });
+      return deferred.promise;
+    };
+
+    Service.forgetPassword = function (data) {
+      var deferred = $q.defer();
+      $http.post(API_PHP + '/users/forget_password', data)
+        .then(function(response) {
+          deferred.resolve(response);
+        }, function(response) {
+          deferred.reject(response);
+        });
+      return deferred.promise;
+    };
+
+    Service.profile = function(data) {
+      var deferred = $q.defer();
+      $http.get(API_PHP + '/users/' + data._id + '?auth_token=' + data.auth_token)
+        .then(function(response) {
+          deferred.resolve(response);
+        });
+      return deferred.promise;
+    };
+
+    return Service;
+  }
+
+  // function AuthService($rootScope, $localStorage, $routeParams, Facebook, GooglePlus, EcoTracker, MemoTracker, LoginService, MolServices) {
+  //   var Service = {};
+
+  //   Service.register = function (data) {
+  //     return LoginService.register(data).then(loginCallback);
+  //   };
+
+  //   Service.login = function (data) {
+  //     return LoginService.login(data).then(loginCallback);
+  //   };
+
+  //   Service.forgetPassword = function (data) {
+  //     return LoginService.forgetPassword(data);
+  //   };
+
+  //   Service.logout = function () {
+  //     $localStorage.$reset();
+  //     delete $localStorage.displayTour;
+  //     $rootScope.$broadcast('event:auth-logoutConfirmed');
+  //   };
+
+  //   Service.FbLogin = function() {
+  //     var deferred = $q.defer();
+
+  //     Facebook.getLoginStatus(facebookLoginStatusReceived);
+
+  //     function facebookLoginStatusReceived(response) {
+  //       if (response.status === 'connected') {
+  //         $localStorage.auth.facebook = response.authResponse;
+  //         deferred.resolve(response);
+  //       } else {
+  //         Facebook.login(facebookLoginCallback)
+  //       }
+  //     }
+
+  //     function facebookLoginCallback(response) {
+  //       if (response.status != 'unknown' || response.status != 'not_authorized') {
+  //         $localStorage.auth.facebook = response.authResponse.accessToken;
+  //         deferred.resolve(response);
+  //       } else {
+  //         deferred.reject(response);
+  //       }
+  //     }
+
+  //     return deferred.promise;
+  //   };
+  //   return Service;
+  // }
+
+  angular.module('login.services', []);
+  angular.module('login.services')
+    .factory('LoginService', ['$http', '$q', '$localStorage', 'API_PHP', LoginFactory]);
+  angular.module('login.services')
+    .factory('AuthService', ['$rootScope', '$localStorage', '$routeParams',
+    'Facebook', 'GooglePlus', 'EcoTracking', 'MemoTracking',
+    'LoginService', 'MolServices', function ($rootScope, $localStorage, $routeParams, Facebook, GooglePlus, EcoTracker, MemoTracker, LoginService, MolServices) {
+      var AuthService = function() {};
+      AuthService.prototype.register = function(data) {
+        return LoginService.register(data).then(loginCallback);
+      };
+
+      AuthService.prototype.login = function(data) {
+        return LoginService.login(data).then(loginCallback);
+      };
+
+      AuthService.prototype.forgetPassword = function(data) {
+        return LoginService.forgetPassword(data);
+      };
+      AuthService.prototype.FbLogin = function() {
+        Facebook.getLoginStatus(function(loginStatusResponse) {
+          if (loginStatusResponse.status === 'connected') {
+            getFbInfo(loginStatusResponse);
+          } else {
+            Facebook.login(function(loginResponse) {
+              if (loginResponse.status === 'unknown' || loginResponse.status === 'not_authorized') {
+
+                $rootScope('event:auth-loginFailed', {
+                  error: "Có lỗi khi đăng nhập bằng Facebook"
+                });
+              } else {
+                getFbInfo(loginResponse);
+              }
+            }, {
+              scope: 'public_profile, email, user_friends'
+            });
+          }
+        });
+      };
+      AuthService.prototype.GLogin = function() {
+        var requestData = {};
+        return GooglePlus.login().then(function(gAuthResult) {
+          requestData.g_access_token = gAuthResult.access_token;
+          GooglePlus.getUser().then(function(response) {
+            requestData.gmail = response.result.email;
+            LoginService.login(requestData).then(loginCallback);
+          });
+        });
+      };
+
+      AuthService.prototype.checkAuth = function() {
+        var data = $localStorage.auth.user;
+        return LoginService.checkAuth(data).then(function(response) {
+          if (response.data.response_code === 401) {
+            $rootScope.$broadcast('event:auth-invalidToken');
+          }
+        });
+      };
+
+      AuthService.prototype.logout = function() {
+        $localStorage.$reset();
+        delete $localStorage.displayTour;
+        $rootScope.$broadcast('event:auth-logoutConfirmed');
+      };
+
+      function getFbInfo(data) {
+        var requestData = {};
+        requestData.fb_access_token = data.authResponse.accessToken;
+        Facebook.api('/me', function(user) {
+          requestData.fb_Id = user.id;
+          requestData.fb_name = user.name;
+          LoginService.login(requestData).then(loginCallback);
+        });
+      }
+
+      function loginCallback(response) {
+        $localStorage.auth = {
+          loggedIn: true,
+          user: response.data,
+          trial: response.data.is_trial
+        };
+
+        var data = angular.fromJson(angular.toJson(response.data));
+        // data.name = data.username;
+        data.name = "memo_" + data._id;
+        delete data.auth_token;
+
+        if (response.data.is_newly_sign_up) {
+          MemoTracker.track('sign up');
+          EcoTracker.track('Web 1.0.2 user logged in', data);
+          $localStorage.displayTour = true;
+        } else {
+          MemoTracker.track('login');
+        }
+        mixpanel.identify(response.data._id);
+
+        var molData = {};
+        molData.code_chanel = $routeParams.code_chanel || -100;
+        molData.id_landingpage = $routeParams.id_landingpage || -100;
+        molData.id_campaign = $routeParams.id_campaign || -100;
+        molData.id_camp_landingpage = $routeParams.id || -100;
+
+        mixpanel.track('Web 1.0.2 user logged in', molData);
+
+        molData.name = data.name || data.username;
+        molData.email = data.email;
+        molData.phone = data.mobile || '';
+
+        MolServices.saveC3(molData);
+
+        $rootScope.$broadcast('event:auth-loginConfirmed', {
+          user: response.data
+        });
+      }
+      return new AuthService();
+    }]);
+}(window.angular));
