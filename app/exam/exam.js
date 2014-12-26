61ab7524f4997b5f7084f476eaadef5f01b25706
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-12-27 00:14:05 +0700

    Fix backspace bug

--- app/exam/exam.js
+++ app/exam/exam.js
@@ -1,21 +1,21 @@
-'use strict';
+(function(angular) {
+  'use strict';
 
-angular.module('exam', [
-    'exam.controllers',
-    'exam.services'
-]).config(['$routeProvider', '$locationProvider', ExamConfig]);
-
-function ExamConfig($routeProvider, $locationProvider) {
+  function ExamConfig($routeProvider) {
     $routeProvider.when('/checkpoint/:checkpoint_position', {
-	templateUrl: 'exam/_index.html',
-	controller: 'ExamCtrl'
+      templateUrl: 'exam/_index.html',
+      controller: 'ExamCtrl'
     });
     $routeProvider.when('/shortcut/:id', {
-	templateUrl: 'exam/_index.html',
-	controller: 'ExamCtrl'
+      templateUrl: 'exam/_index.html',
+      controller: 'ExamCtrl'
     });
     $routeProvider.when('/skill/:id/:lesson_number', {
-	templateUrl: 'exam/_index.html',
-	controller: 'ExamCtrl'
+      templateUrl: 'exam/_index.html',
+      controller: 'ExamCtrl'
     });
-}
+  }
+
+  angular.module('exam', ['exam.controllers', 'exam.services'])
+    .config(['$routeProvider', ExamConfig]);
+}(window.angular));
