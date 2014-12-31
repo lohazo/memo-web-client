0e28daf546650f5839b0d3c9b99ab3535b020c16
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-12-31 14:15:23 +0700

    Fix dictionary bug

--- app/components/words/words.js
+++ app/components/words/words.js
@@ -1,38 +1,43 @@
-(function (angular) {
+(function(angular) {
   'use strict';
 
   function WordsFactory($http, $q, $localStorage, API, API_PHP) {
-    var httpConfig = {ignoreLoadingBar: true};
+    var httpConfig = {
+      ignoreLoadingBar: true
+    };
     var Factory = {};
 
-    Factory.getWord = function (wordToSearch) {
-      var words = $localStorage.words.words;
+    Factory.getWord = function(wordToSearch) {
+      var words = [];
+      if ($localStorage.words) {
+        words = $localStorage.words.words;
+      }
       var lang = $localStorage.auth.user.current_course_id;
-      return words.filter(function (word) {
+      return words.filter(function(word) {
         return (lang + '_' + wordToSearch) === word._id;
       })[0];
     };
 
-    Factory.getWords = function () {
+    Factory.getWords = function() {
 
       var deferred = $q.defer();
       var words = $localStorage.words || {};
       var version = words.version || 0;
 
       $http.get(API + '/shared_settings/all_words.json?version=' + version, httpConfig)
-        .then(function (response) {
+        .then(function(response) {
           if (version != response.data.version) {
             $localStorage.words = response.data;
           }
           deferred.resolve(response);
-        }, function (response) {
+        }, function(response) {
           deferred.reject(response);
         });
 
       return deferred.promise;
     };
 
-    Factory.revealWords = function (data) {
+    Factory.revealWords = function(data) {
       // data = {words: ['en-vi_a', 'en-vi_x']}
       var deferred = $q.defer();
       var authToken = $localStorage.auth.user.auth_token;
@@ -40,9 +45,9 @@
       data.auth_token = authToken;
 
       $http.post(API_PHP + '/words?', data, httpConfig)
-        .then(function (response) {
+        .then(function(response) {
           deferred.resolve(response);
-        }, function (response) {
+        }, function(response) {
           deferred.reject(response);
         });
 
