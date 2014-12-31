7593d965d18447af673efee22f6a2e6d23b2d37f
Nguyễn Phan Hải <hainp@topica.edu.vn>
2014-12-31 19:49:47 +0700

    Module referral

--- app/components/referral/controllers.js
+++ app/components/referral/controllers.js
@@ -2,14 +2,20 @@
  * Referral Controllers
  */
 
-(function(angular){
+(function(angular) {
   'use strict';
+
+  function ReferralCtrl($scope, ReferralService) {
+
+  }
+
   function ReferralStatusCtrl(service) {
-    service.getStatus().then(function(res){
+    service.getStatus().then(function(res) {
       console.log(res.data);
     });
   }
 
   angular.module('referral.controllers', [])
-        .controller('ReferralStatusCtrl', ['ReferralService', ReferralStatusCtrl])
+    .controller('ReferralCtrl', ['ReferralService', ReferralCtrl])
+    .controller('ReferralStatusCtrl', ['ReferralService', ReferralStatusCtrl])
 })(window.angular);
