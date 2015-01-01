b395f6062e594996943000f0431d283b4d104520
Dao Thanh Luan <luandt@topica.edu.vn>
2015-01-01 15:15:00 +0700

    Dung layout screen 2

--- app/components/referral/directives.js
+++ app/components/referral/directives.js
@@ -8,8 +8,36 @@
     .directive('status', function(){
       return {
         strict: 'EA',
-        controller: 'ReferralStatusCtrl',
+        controller: 'ReferralStatusCtrl'
         // templateUrl: 'components/landingpage/_header.html'
       };
-    });
+    })
+
+     .directive('referralMain', function(){
+        return {
+          strict: 'EA',
+          templateUrl: 'components/referral/_main.html'
+        }
+      })
+
+    .directive('screenTop', function(){
+        return {
+          strict: 'EA',
+          templateUrl: 'components/referral/_main-top-Screen-2.html'
+        }
+      })
+
+    .directive('screenCenter', function(){
+        return {
+          strict: 'EA',
+          templateUrl: 'components/referral/_main-center-Screen-2.html'
+        }
+      })
+
+    .directive('screenBottom', function(){
+        return {
+          strict: 'EA',
+          templateUrl: 'components/referral/_main-bottom-Screen-2.html'
+        }
+      });
 })(window.angular);
