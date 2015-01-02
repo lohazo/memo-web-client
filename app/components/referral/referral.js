4e7c7cea60c4f1664fc8e233d0c40b39148069fe
namct <nammnh@topica.edu.vn>
2015-01-02 22:32:14 +0700

    dev screen-2

--- app/components/referral/referral.js
+++ app/components/referral/referral.js
@@ -3,7 +3,7 @@
 
   function ReferralConfig($routeProvider) {
     $routeProvider.when('/referral', {
-      templateUrl: 'components/referral/_entercode.html',
+      templateUrl: 'components/referral/_main.html',
       controller: 'ReferralCtrl'
     });
   }
