02ff1b1dd6fbb7c2882456604b39aa54776b468c
Dang Tuan Anh <anhdt4@topica.edu.vn>
2015-01-03 18:27:10 +0700

    referral flow

--- app/components/referral/referral.js
+++ app/components/referral/referral.js
@@ -2,19 +2,24 @@
   'use strict';
 
   function ReferralConfig($routeProvider) {
-    $routeProvider.when('/referral', {
-      templateUrl: 'components/referral/_entercode.html',
-      controller: 'ReferralCtrl',
-      // resolve: {
-      //   getStatus: function(ReferralService) {
-      //     return ReferralService.getStatus();
-      //   },
-      //   getDetail: function(Profile) {
-      //     // console.log(Profile);
-      //     return Profile.detail;
-      //   }
-      // }
-    });
+    $routeProvider
+      .when('/referral', {
+        templateUrl: 'components/referral/_index.html',
+        controller: 'ReferralCtrl',
+        // resolve: {
+        //   getStatus: function(ReferralService) {
+        //     return ReferralService.getStatus();
+        //   },
+        //   getDetail: function(Profile) {
+        //     // console.log(Profile);
+        //     return Profile.detail;
+        //   }
+        // }
+      })
+      .when('/referral/profile', {
+        templateUrl: 'components/referral/_entercode.html',
+        controller: 'ReferralEntercodeCtrl',
+      })
   }
 
   angular.module('referral', [
