87b0b1936f7d455796539cc479bfd4ffe8dae2c4
Dang Tuan Anh <anhdt4@topica.edu.vn>
2015-01-04 09:59:15 +0700

    referral clear directive

--- app/components/referral/directives.js
+++ app/components/referral/directives.js
@@ -9,14 +9,6 @@
       return {
         strict: 'EA',
         controller: 'ReferralCtrl'
-        // templateUrl: 'components/landingpage/_header.html'
-      };
-    })
-    .directive('referralHeader', function(){
-      return {
-        strict: 'EA',
-        controller: 'ReferralHeaderCtrl'
-        // templateUrl: 'components/landingpage/_header.html'
       };
     })
     .directive('referralBody', function(){
@@ -26,87 +18,22 @@
         templateUrl: 'components/referral/_body.html'
       };
     })
-    // .directive('referralBodyOne', function(){
-    //   return {
-    //     strict: 'EA',
-    //     controller: 'ReferralBodyCtrl',
-    //     templateUrl: 'components/referral/_step_one.html'
-    //   };
-    // })
     .directive('referralFooter', function(){
       return {
         strict: 'EA',
         controller: 'ReferralFooterCtrl'
-        // templateUrl: 'components/landingpage/_header.html'
       };
     })
     .directive('referralEntercodeCtrl', function(){
       return {
         strict: 'EA',
         controller: 'ReferralEntercodeCtrl',
-        //templateUrl: 'components/landingpage/_entercode.html'
       };
     })
-
-     .directive('referralMain', function(){
-        return {
-          strict: 'EA',
-          templateUrl: 'components/referral/_main.html'
-        }
-      })
-
-    .directive('screenTop', function(){
-        return {
-          strict: 'EA',
-          templateUrl: 'components/referral/_main-top-Screen-2.html'
-        }
-      })
-
-    .directive('screenCenter', function(){
-        return {
-          strict: 'EA',
-          templateUrl: 'components/referral/_main-center-Screen-2.html'
-        }
-      })
-
-    .directive('screenBottom', function(){
-        return {
-          strict: 'EA',
-          templateUrl: 'components/referral/_main-bottom-Screen-2.html'
-        }
-      })
-
-    .directive('referralMainScreen3', function(){
-          return {
-              strict: 'EA',
-              templateUrl: 'components/referral/_screen-3-main.html'
-          }
-      })
-
-    .directive('referralTopScreen3', function(){
-          return {
-              strict: 'EA',
-              templateUrl: 'components/referral/_screen-3-top.html'
-          }
-      })
-
-    .directive('referralCenterScreen3', function(){
-          return {
-              strict: 'EA',
-              templateUrl: 'components/referral/_screen-3-center.html'
-          }
-      })
-
-    .directive('referralBottomScreen3', function(){
-        return {
-            strict: 'EA',
-            templateUrl: 'components/referral/_screen-3-bottom.html'
-        }
-    })
     .directive('submitcodeModal', function() {
-        return {
-            strict: 'EA',
-            templateUrl: 'components/referral/_submitcode_modal.html'
-        }
+      return {
+        strict: 'EA',
+        templateUrl: 'components/referral/_submitcode_modal.html'
+      }
     })
 })(window.angular);
