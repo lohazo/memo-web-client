6ec96b4e1a52152696b85aa37ee148a7d4de8fd6
Nguyễn Phan Hải <hainp@topica.edu.vn>
2015-01-05 18:46:43 +0700

    Start tracking

--- app/components/tracking/tracking.js
+++ app/components/tracking/tracking.js
@@ -1,5 +1,33 @@
-'use strict';
+(function(angular) {
+  'use strict';
 
-angular.module('tracking', [
-   'tracking.services' 
-]);
+  function clickEventTrack() {
+    return {
+      strict: 'EA',
+      scope: {
+        trackingData: "@"
+      },
+      controller: 'ClickEventTrackCtrl',
+      link: function($scope, $element) {
+        $element.bind('click', function() {
+          $scope.click();
+        });
+      }
+    };
+  }
+
+  function ClickEventTrackCtrl($scope, EcoTracker) {
+    var data = angular.fromJson($scope.trackingData);
+    $scope.click = function() {
+      EcoTracker.campaignTrack("Web 1.0.2 click event track", data);
+    };
+  }
+
+  angular.module('tracking', [
+    'tracking.services'
+  ]);
+
+  angular.module('tracking')
+    .directive('clickEventTrack', clickEventTrack)
+    .controller('ClickEventTrackCtrl', ['$scope', 'EcoTracking', ClickEventTrackCtrl]);
+}(window.angular));
