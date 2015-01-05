ecbcbb8ee10af945a3c1b7f984d9435b857ccd97
Nguyễn Phan Hải <hainp@topica.edu.vn>
2015-01-05 16:02:39 +0700

    Fix register bug

--- app/components/landingpage/header/header.js
+++ app/components/landingpage/header/header.js
@@ -119,20 +119,26 @@
 
     $scope.register = function() {
       var user = angular.fromJson(angular.toJson($scope.user));
-      AuthService.checkCode({
-          referral_code: user.referral_code
-        })
-        .then(function() {
-          delete user.password;
-          delete $scope.user.referral_code;
-          AuthService.register($scope.user)
-            .then(closeModal, displayMessageOnFail)
-            .then(function() {
-              AuthService.submitReferralCode({
-                referral_code: user.referral_code
-              });
-            }, displayMessageOnFail);
-        }, displayMessageOnFail);
+      delete user.password;
+      if (user.referral_code && user.referral_code !== "") {
+        AuthService.checkCode({
+            referral_code: user.referral_code
+          })
+          .then(function() {
+            delete $scope.user.referral_code;
+            AuthService.register($scope.user)
+              .then(closeModal, displayMessageOnFail)
+              .then(function() {
+                AuthService.submitReferralCode({
+                  referral_code: user.referral_code
+                });
+              }, displayMessageOnFail);
+          }, displayMessageOnFail);
+      } else {
+        delete $scope.user.referral_code;
+        AuthService.register($scope.user)
+          .then(closeModal, displayMessageOnFail);
+      }
     };
 
     $scope.login = function() {
