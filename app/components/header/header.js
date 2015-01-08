6908d87d7aa881011d34819b09dcb2306f44fba3
Nguyễn Phan Hải <hainp@topica.edu.vn>
2015-01-08 20:36:13 +0700

    Update code

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -1,26 +1,27 @@
-(function (angular) {
+(function(angular) {
   'use strict';
 
   function HeaderCtrl($scope, $rootScope, $location, AuthService) {
-        $scope.getActiveItem = function (path) {
-          if ($location.path().substr(0, path.length) == path) {
-            return "active";
-          } else {
-            return "";
-          }
-        };
-        $scope.logout = function() {
-          AuthService.logout();
-        };
-        $scope.menus = [{
-            'title': 'Trang chủ',
-            'link': '/'
-          },
-          // {'title': 'Thảo luận', 'link': '/discussion'}
-        ];
+    $scope.getActiveItem = function(path) {
+      if ('/' + $location.path().split('/')[0] === path) {
+        return "active";
+      } else {
+        return "";
+      }
+    };
+    $scope.logout = function() {
+      AuthService.logout();
+    };
+    $scope.menus = [{
+        'title': 'Trang chủ',
+        'link': '/'
+      },
+      // {'title': 'Thảo luận', 'link': '/discussion'}
+    ];
   }
 
   angular.module('header', []);
   angular.module('header').controller('HeaderCtrl', [
-    '$rootScope', '$scope', '$location', 'AuthService', HeaderCtrl]);
+    '$rootScope', '$scope', '$location', 'AuthService', HeaderCtrl
+  ]);
 }(window.angular));
