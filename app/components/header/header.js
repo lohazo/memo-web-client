eb89dce7a407ad403666339bfdcbd014a6f75bd5
namct <nammnh@topica.edu.vn>
2015-01-08 16:39:57 +0700

    add popup show keyboard

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -1,7 +1,7 @@
 (function (angular) {
   'use strict';
 
-  function HeaderCtrl($scope, $rootScope, $location, AuthService) {
+  function HeaderCtrl($scope, $rootScope, $location, AuthService, $modal) {
         $scope.getActiveItem = function (path) {
           if ($location.path().substr(0, path.length) == path) {
             return "active";
@@ -18,9 +18,23 @@
           },
           // {'title': 'Thảo luận', 'link': '/discussion'}
         ];
+
+          $scope.showShortcutPopup = function() {
+            var modalInstance = $modal.open({
+              templateUrl: 'components/header/_keyboard.html',
+              // controller: 'LoginModalInstanceCtrl',
+              windowClass: 'box-shortcuts"'
+            });
+
+            modalInstance.result.then(function(msg) {
+              if ($scope[msg] instanceof Function) $scope[msg]();
+            });
+          };
+
   }
+
 
   angular.module('header', []);
   angular.module('header').controller('HeaderCtrl', [
-    '$rootScope', '$scope', '$location', 'AuthService', HeaderCtrl]);
+    '$rootScope', '$scope', '$location', 'AuthService', '$modal', HeaderCtrl]);
 }(window.angular));
