5a7aac5741905cd1a0e7be5fed7d3ce13ce8f699
Nguyễn Phan Hải <hainp@topica.edu.vn>
2015-01-14 14:26:38 +0700

    feat(notification): refactor

--- app/components/notification/referral/newTier.js
+++ app/components/notification/referral/newTier.js
@@ -14,14 +14,15 @@
     };
   }
 
-  function AchieveNewTierCtrl($scope) {
+  function AchieveNewTierCtrl($scope, AppSetting) {
     var gift = angular.fromJson($scope.gift);
+    $scope.iconUrl = AppSetting.sharedSettings.notification_icon_urls.referral;
     $scope.message = $scope.content.replace('{new_gift_value}', gift.new_gift_value);
   }
 
   angular.module('notification.newTier', []);
   angular.module('notification.newTier')
-    .controller('AchieveNewTierCtrl', ['$scope', AchieveNewTierCtrl]);
+    .controller('AchieveNewTierCtrl', ['$scope', 'AppSetting', AchieveNewTierCtrl]);
   angular.module('notification.newTier')
     .directive('notificationNewTier', NotificationNewTier);
 }(window.angular));
