7ae886c4a06c02c434f9b6091aebfe42ac9bec4a
Dao Thanh Luan <luandt@topica.edu.vn>
2015-01-28 21:59:39 +0700

    fix leaderboard friend

--- app/leaderboard/controllers.js
+++ app/leaderboard/controllers.js
@@ -60,15 +60,15 @@
       if ($scope.profile) {
         $scope.tabs = [{
           'title': "Tuần này",
-          'users': $scope.profile.followings_leaderboard_by_week,
+          'users': $scope.profile.leader_board_by_week,
           'active': true
         }, {
           'title': "Tháng này",
-          'users': $scope.profile.followings_leaderboard_by_month,
+          'users': $scope.profile.leader_board_by_month,
           'active': false
         }, {
           'title': "Tổng cộng",
-          'users': $scope.profile.followings_leaderboard_all_time,
+          'users': $scope.profile.leader_board_all_time,
           'active': false
         }];
       }
