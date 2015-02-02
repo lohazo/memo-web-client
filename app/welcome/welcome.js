eeebd7f1c04ce2c4e87714780bc7c189d6da4190
Nguyễn Phan Hải <hainp@topica.edu.vn>
2015-02-02 17:44:53 +0700

    feat(tutorial): integrate tutorial APIs

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -5,7 +5,12 @@
   function WelcomeConfig($routeProvider) {
     $routeProvider.when('/welcome', {
       templateUrl: 'welcome/_index.html',
-      controller: 'WelcomeCtrl'
+      controller: 'WelcomeCtrl',
+      resolve: {
+        welcomeData: function (Welcome) {
+          return Welcome.start();
+        }
+      }
     });
   }
 
@@ -13,7 +18,8 @@
 
   }
 
-  angular.module('welcome', ['app.services', 'welcome.quitLink']).config(['$routeProvider',
+  angular.module('welcome', ['app.services', 'welcome.quitLink', 'welcome.services']).config([
+    '$routeProvider',
     WelcomeConfig
   ]);
   angular.module('welcome')
---
+++ app/welcome/welcome.services.js
@@ -0,0 +1,116 @@
+(function (angular) {
+  'use strict';
+
+  function WelcomeServices($http, $localStorage, API) {
+    var Services = {};
+
+    Services.start = function () {
+      var data = {
+        auth_token: $localStorage.auth.user.auth_token,
+        current_course_id: $localStorage.auth.user.current_course_id
+      };
+
+      return $http.get(API + '/tutorial/start?platform=web&current_course_id=' + data.current_course_id +
+        '&auth_token=' + data.auth_token);
+    };
+
+    /*
+     * data = {current_step, next_step_token}
+     */
+    Services.nextStep = function (data) {
+      var userId = $localStorage.auth.user._id;
+      data.auth_token = $localStorage.auth.user.auth_token;
+
+      return $http.post(API + '/tutorial/' + userId + '/next_step', data);
+    };
+
+    /*
+     * data = {current_step}
+     */
+    Services.claimBonus = function (data) {
+      var userId = $localStorage.auth.user._id;
+      data.auth_token = $localStorage.auth.user.auth_token;
+
+      return $http.post(API + '/tutorial/' + userId + '/claim_bonus', data);
+    };
+
+    /*
+     * data = {current_step}
+     */
+    Services.skip = function (data) {
+      var userId = $localStorage.auth.user._id;
+      data.auth_token = $localStorage.auth.user.auth_token;
+
+      return $http.post(API + '/tutorial/' + userId + '/skip', data);
+    };
+
+    Services.finish = function () {
+      var data = {
+        auth_token: $localStorage.auth.user.auth_token
+      };
+
+      return $http.post(API + '/tutorial/' + userId + '/finish', data);
+    };
+
+    return Services;
+  }
+
+  function Welcome(WelcomeServices) {
+    var Services = {};
+
+    function init() {
+      // currentStep = ant's position
+      Services.currentStep = 0;
+      Services.answeredSteps = 0;
+      Services.exam = {};
+    }
+
+    Services.start = function () {
+      init();
+      return WelcomeServices.start().then(function (response) {
+        Services.exam = response.data;
+        Services.currentStep += 1;
+      });
+    };
+
+    Services.answer = function () {
+      Services.answeredSteps += 1;
+    };
+
+    Services.nextStep = function () {
+      return WelcomeServices.nextStep({
+        current_step: Services.currentStep,
+        next_step_token: Services.exam.next_step_token
+      }).then(function (response) {
+        Services.currentStep += 1;
+        Services.next_step_token = response.data.next_step_token;
+      })
+    };
+
+    Services.claimBonus = function () {
+      return WelcomeServices.claimBonus({
+        current_step: Services.currentStep
+      });
+    };
+
+    Services.skip = function () {
+      return WelcomeServices.skip({
+        current_step: Services.currentStep
+      }).then(function (response) {
+        init();
+      });
+    };
+
+    Services.finish = function () {
+      init();
+      return WelcomeServices.finish();
+    };
+
+    return Services;
+  }
+
+  angular.module('welcome.services', []);
+  angular.module('welcome.services')
+    .factory('WelcomeServices', ['$http', '$localStorage', 'API', WelcomeServices])
+    .factory('Welcome', ['WelcomeServices', Welcome]);
+}(window.angular));
