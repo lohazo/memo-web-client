77705ecb9b33a8bde27f5c3b7403466b584942f4
Nam ct <kantaeyoung@gmail.com>
2015-02-06 15:29:19 +0700

    tich hop keycode === 13

--- app/welcome/player/player.js
+++ app/welcome/player/player.js
@@ -1,7 +1,7 @@
 (function (angular) {
   'use strict';
 
-  function WelcomePlayerCtrl($scope, $location) {
+  function WelcomePlayerCtrl($scope, $location, $element) {
     var ctrl = this;
     ctrl.data = $scope.welcomePlayer;
     ctrl.click = function () {
@@ -21,6 +21,13 @@
         $location.path('/');
       });
     };
+    $element[0].focus();
+    $element.bind('keydown', function (e) {
+      if (e.keyCode === 13) {
+        // 'Enter'
+        ctrl.click();
+      }
+    });
   }
 
   function WelcomePlayerHeaderCtrl($scope) {
@@ -47,22 +54,22 @@
 
   angular.module('welcome.player', []);
   angular.module('welcome.player')
-    .controller('WelcomePlayerCtrl', ['$scope', '$location', WelcomePlayerCtrl])
-    .controller('WelcomePlayerHeaderCtrl', ['$scope', WelcomePlayerHeaderCtrl])
-    .controller('WelcomePlayerFooterCtrl', ['$scope', WelcomePlayerFooterCtrl])
-    .directive('welcomePlayerContainer', function () {
-      return {
-        strict: 'EA',
-        scope: {
-          welcomePlayer: "=player",
-          click: "&"
-        },
-        controller: 'WelcomePlayerCtrl',
-        controllerAs: 'player',
-        link: function ($scope, $element, $attrs) {
-          $element[0].focus();
-          $element.bind('keydown', function (e) {
-            if (e.keyCode === 13) {
+  .controller('WelcomePlayerCtrl', ['$scope', '$location', '$element', WelcomePlayerCtrl])
+  .controller('WelcomePlayerHeaderCtrl', ['$scope', WelcomePlayerHeaderCtrl])
+  .controller('WelcomePlayerFooterCtrl', ['$scope', WelcomePlayerFooterCtrl])
+  .directive('welcomePlayerContainer', function () {
+    return {
+      strict: 'EA',
+      scope: {
+        welcomePlayer: "=player",
+        click: "&"
+      },
+      controller: 'WelcomePlayerCtrl',
+      controllerAs: 'player',
+      link: function ($scope, $element, $attrs) {
+        $element[0].focus();
+        $element.bind('keydown', function (e) {
+          if (e.keyCode === 13) {
               // 'Enter'
               $scope.click();
               $scope.$apply();
@@ -73,35 +80,35 @@
               // return false;
             }
           });
-        },
-        templateUrl: 'welcome/player/_player-container.html'
-      };
-    })
-    .directive('welcomePlayerHeader', function () {
-      return {
-        strict: 'EA',
-        scope: {
-          settings: "=",
-          currentStep: "=",
-          answeredSteps: "=",
-          skip: "&"
-        },
-        controller: 'WelcomePlayerHeaderCtrl',
-        controllerAs: 'playerHeader',
-        templateUrl: 'welcome/player/_player-header.html',
-      };
-    })
-    .directive('welcomePlayerFooter', function () {
-      return {
-        strict: 'EA',
-        scope: {
-          settings: "=",
-          question: "=",
-          click: "&"
-        },
-        controller: 'WelcomePlayerFooterCtrl',
-        controllerAs: 'playerFooter',
-        templateUrl: 'welcome/player/_player-footer.html'
-      };
-    });
+      },
+      templateUrl: 'welcome/player/_player-container.html'
+    };
+  })
+  .directive('welcomePlayerHeader', function () {
+    return {
+      strict: 'EA',
+      scope: {
+        settings: "=",
+        currentStep: "=",
+        answeredSteps: "=",
+        skip: "&"
+      },
+      controller: 'WelcomePlayerHeaderCtrl',
+      controllerAs: 'playerHeader',
+      templateUrl: 'welcome/player/_player-header.html',
+    };
+  })
+  .directive('welcomePlayerFooter', function () {
+    return {
+      strict: 'EA',
+      scope: {
+        settings: "=",
+        question: "=",
+        click: "&"
+      },
+      controller: 'WelcomePlayerFooterCtrl',
+      controllerAs: 'playerFooter',
+      templateUrl: 'welcome/player/_player-footer.html'
+    };
+  });
 }(window.angular));
