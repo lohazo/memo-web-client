70d754eeca6910e21ef3aab1394daef86c212622
Dao Thanh Luan <luandt@topica.edu.vn>
2015-02-27 16:28:14 +0700

    fix(settings): fix moment-study

--- app/settings/profile.js
+++ app/settings/profile.js
@@ -117,9 +117,6 @@
     function updateUser() {
       $scope.profile = Profile.detail;
       $scope.user = Profile.user;
-      console.log(Profile.user.created_at);
-      var date = new Date(Profile.user.created_at);
-      $scope.test_date = date;
     }
 
     $scope.linkFb = function () {
