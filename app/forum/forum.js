d56e68e3d258f23f2a617c88060e97e88df79367
Dao Thanh Luan <luandt@topica.edu.vn>
2015-03-18 00:46:20 +0700

    feat(PostDetail): dung layout cho phan PostDetail

--- app/forum/forum.js
+++ app/forum/forum.js
@@ -7,6 +7,10 @@
 			templateUrl: 'forum/_create-post.html',
 			controller: ''
 		});
+		$routeProvider.when('/forum/postDetail', {
+			templateUrl: 'forum/_post-detail.html',
+			controller: ''
+		});
 	}
 
 	angular.module('forum', [])
