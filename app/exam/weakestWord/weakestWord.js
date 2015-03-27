3ba315248e477e6fa2479770477f656c4bf43901
Dao Thanh Luan <luandt@topica.edu.vn>
2015-03-27 14:38:39 +0700

    feat(weakest-word): feat controller,service for weakest-word

--- app/exam/weakestWord/weakestWord.js
+++ app/exam/weakestWord/weakestWord.js
@@ -4,15 +4,51 @@
   function weakestWordConfig($routeProvider) {
   	$routeProvider.when('/weakestword', {
   		templateUrl: 'exam/weakestWord/_weakest-word.html',
-  		// controller: 'WeakestWordCtrl'
+  		controller: 'ListWeakestWordCtrl'
   	});
-  	$routeProvider.when('/weakestword/detail', {
+  	$routeProvider.when('/weakestword/:id', {
   		templateUrl: 'exam/weakestWord/_weakest-word-detail.html',
-  		// controller: 'WeakestWordCtrl'
+  		controller: 'WeakestWordDetailCtrl'
   	});
-  };
+  }
+
+  function ListWeakestWordCtrl($scope, WeakestWordServices) {
+    WeakestWordServices.listWeakestWord().success(function (data) {
+      $scope.weakestWords = data;
+    });
+  }
+
+  function WeakestWordDetailCtrl($scope, WeakestWordServices) {
+    WeakestWordServices.listWeakestWord().success(function (data) {
+      $scope.weakestWords = data;
+    });
+  }
+
+  function WeakestWordServices($http, $q, $localStorage, API) {
+    var Services = {};
+
+    /*
+     * data = {skill_id: , page: }
+     */
+    Services.listWeakestWord = function(data) {
+      var authToken = $localStorage.auth.user.auth_token;
+      var userId = $localStorage.auth.user._id;
+
+      var endpoint = API + '/users/' + userId + '/weakest_word' + '?platform=web&auth_token=' + authToken;
+
+      // if (data.page) endpoint += '&page=' + data.page;
+      // if (data.skill_id) endpoint += '&sort=' + data.skill_id;
+
+      return $http.get(endpoint);
+    };
+
+    return Services;
+  }
   
 
   angular.module('weakestWord', [])
     .config(['$routeProvider', weakestWordConfig])
+    .controller('ListWeakestWordCtrl', ['$scope', 'WeakestWordServices', ListWeakestWordCtrl])
+    .controller('WeakestWordDetailCtrl', ['$scope', 'WeakestWordServices', WeakestWordDetailCtrl])
+    .factory('WeakestWordServices', ['$http', '$q', '$localStorage', 'API', WeakestWordServices])
 }(window.angular));
