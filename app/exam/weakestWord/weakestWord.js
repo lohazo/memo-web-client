6e395468e63c4e26e7f9f7ebd6887dc5bbc4c434
Dao Thanh Luan <luandt@topica.edu.vn>
2015-03-27 18:03:38 +0700

    feat(weakest-word): add data for weakest-words

--- app/exam/weakestWord/weakestWord.js
+++ app/exam/weakestWord/weakestWord.js
@@ -13,15 +13,278 @@
   }
 
   function ListWeakestWordCtrl($scope, WeakestWordServices) {
-    WeakestWordServices.listWeakestWord().success(function (data) {
-      $scope.weakestWords = data;
-    });
-  }
+    // WeakestWordServices.listWeakestWord().success(function (data) {
+    // });
+    $scope.weakest_words = {
+      "words":[
+        {"text": "a",
+         "skills": "Cơ bản 1",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 4,
+         "translation":[
+           {"text": "một"},
+           {"text": "một cái"},
+           {"text": "một vài"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "một",
+         "examples": [
+           {"text": "I am a girl"},
+           {"text": "She is a girl"}
+          ],
+        },
+        {"text": "an",
+         "skills": "Cơ bản 2",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 2,
+         "translation":[
+           {"text": "một"},
+           {"text": "một vài"},
+           {"text": "một cái"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "một",
+         "examples": [
+           {"text": "I have an apple"},
+           {"text": "She has an orange"}
+          ],
+        },
+        {"text": "the",
+         "skills": "Cụm từ",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 1,
+         "translation":[
+           {"text": "một"},
+           {"text": "một vài"},
+           {"text": "một cái"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "một",
+         "examples": [
+           {"text": "I have an apple"},
+           {"text": "She has an orange"}
+          ],
+        },
+        {"text": "man",
+         "skills": "Món ăn",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 3,
+         "translation":[
+           {"text": "đàn ông"},
+           {"text": "quý ông"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "đàn ông",
+         "examples": [
+           {"text": "I am a man"},
+           {"text": "The man is running"}
+          ],
+        },
+        {"text": "woman",
+         "skills": "Số nhiều",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 4,
+         "translation":[
+           {"text": "phụ nữ"},
+           {"text": "quý bà"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "phụ nữ",
+         "examples": [
+           {"text": "She is a woman"},
+           {"text": "Woman is beautiful as flower"}
+          ],
+        },
+        {"text": "boy",
+         "skills": "Sở hữu",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 2,
+         "translation":[
+           {"text": "cậu bé"},
+           {"text": "con trai"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "cậu bé",
+         "examples": [
+           {"text": "He is a boy"},
+           {"text": "He is my boyfriend"}
+          ],
+        },
+        {"text": "girl",
+         "skills": "Màu sắc",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 1,
+         "translation":[
+           {"text": "cô bé"},
+           {"text": "con gái"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "cô bé",
+         "examples": [
+           {"text": "She is a girl"},
+           {"text": "She is my girlfriend"}
+          ],
+        },
+        {"text": "refrigerator",
+         "skills": "Giới từ",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 4,
+         "translation":[
+           {"text": "cô bé"},
+           {"text": "con gái"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "cô bé",
+         "examples": [
+           {"text": "She is a girl"},
+           {"text": "She is my girlfriend"}
+          ],
+        },
+      ],
+      "next_page":2,
+      "total_page":2,
+      "title":"Từ vựng yếu",
+      "content":"",
+      "submit_button":"CỦNG CỐ"
+      }
+    };
 
   function WeakestWordDetailCtrl($scope, WeakestWordServices) {
-    WeakestWordServices.listWeakestWord().success(function (data) {
-      $scope.weakestWords = data;
-    });
+    // WeakestWordServices.listWeakestWord().success(function (data) {
+    // });
+    $scope.weakest_words = {
+      "words":[
+        {"text": "a",
+         "skills": "Cơ bản 1",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 4,
+         "translation":[
+           {"text": "một"},
+           {"text": "một cái"},
+           {"text": "một vài"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "một",
+         "examples": [
+           {"text": "I am a girl"},
+           {"text": "She is a girl"}
+          ],
+        },
+        {"text": "an",
+         "skills": "Cơ bản 2",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 2,
+         "translation":[
+           {"text": "một"},
+           {"text": "một vài"},
+           {"text": "một cái"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "một",
+         "examples": [
+           {"text": "I have an apple"},
+           {"text": "She has an orange"}
+          ],
+        },
+        {"text": "the",
+         "skills": "Cụm từ",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 1,
+         "translation":[
+           {"text": "một"},
+           {"text": "một vài"},
+           {"text": "một cái"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "một",
+         "examples": [
+           {"text": "I have an apple"},
+           {"text": "She has an orange"}
+          ],
+        },
+        {"text": "man",
+         "skills": "Món ăn",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 3,
+         "translation":[
+           {"text": "đàn ông"},
+           {"text": "quý ông"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "đàn ông",
+         "examples": [
+           {"text": "I am a man"},
+           {"text": "The man is running"}
+          ],
+        },
+        {"text": "woman",
+         "skills": "Số nhiều",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 4,
+         "translation":[
+           {"text": "phụ nữ"},
+           {"text": "quý bà"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "phụ nữ",
+         "examples": [
+           {"text": "She is a woman"},
+           {"text": "Woman is beautiful as flower"}
+          ],
+        },
+        {"text": "boy",
+         "skills": "Sở hữu",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 2,
+         "translation":[
+           {"text": "cậu bé"},
+           {"text": "con trai"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "cậu bé",
+         "examples": [
+           {"text": "He is a boy"},
+           {"text": "He is my boyfriend"}
+          ],
+        },
+        {"text": "girl",
+         "skills": "Màu sắc",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 1,
+         "translation":[
+           {"text": "cô bé"},
+           {"text": "con gái"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "cô bé",
+         "examples": [
+           {"text": "She is a girl"},
+           {"text": "She is my girlfriend"}
+          ],
+        },
+        {"text": "refrigerator",
+         "skills": "Giới từ",
+         "last_study": "10-03-2015, 17:25:11",
+         "strength_gap": 4,
+         "translation":[
+           {"text": "cô bé"},
+           {"text": "con gái"}
+          ],
+         "speaker": "http://admin.memo.edu.vn/uploads/sentence/audio/normal/normal-54d2f4316d61696c798e0800.mp3",
+         "maining": "cô bé",
+         "examples": [
+           {"text": "She is a girl"},
+           {"text": "She is my girlfriend"}
+          ],
+        },
+      ],
+      "next_page":2,
+      "total_page":2,
+      "title":"Từ vựng yếu",
+      "content":"",
+      "submit_button":"CỦNG CỐ"
+      }
+    };
   }
 
   function WeakestWordServices($http, $q, $localStorage, API) {
