18a71b35053bda15b0e6cc565ca8ad75293f9222
Dao Thanh Luan <luandt@topica.edu.vn>
2015-03-30 17:22:12 +0700

    fix(weakest-word): fix construct

--- app/exam/weakestWord/weakestWord.js
+++ app/exam/weakestWord/weakestWord.js
@@ -6,38 +6,9 @@
   		templateUrl: 'exam/weakestWord/_weakest-word.html',
   		controller: 'ListWeakestWordCtrl'
   	});
-  	$routeProvider.when('/weakestword/:id', {
-  		templateUrl: 'exam/weakestWord/_weakest-word-detail.html',
-  		controller: 'WeakestWordDetailCtrl',
-      resolve: {
-        Word: function ($route, WeakestWordServices) {
-          return WeakestWordServices.listWeakestWord({
-            id: $route.current.params.id
-          });
-        }
-      }
-  	});
   }
 
   function ListWeakestWordCtrl($scope, $location, WeakestWordServices) {
-    $scope.max_page = 5;
-
-    WeakestWordServices.listWeakestWord().success(function (data) {
-      $scope.weakest_word = data;
-      $scope.total_items = data.total_page * 10;
-      $scope.currentPage = data.next_page - 1;
-    });
-
-    $scope.setPage = function (page) {
-      $scope.weakest_word.page = page;
-      WeakestWordServices.listWeakestWord($scope.weakest_word).success(function (data) {
-        $scope.weakest_word = data;
-      });
-      return;
-    };
-  }
-
-  function WeakestWordDetailCtrl($scope, $location, WeakestWordServices) {
     $scope.max_page = 5;
 
     WeakestWordServices.listWeakestWord().success(function (data) {
@@ -79,6 +50,5 @@
   angular.module('weakestWord', [])
     .config(['$routeProvider', weakestWordConfig])
     .controller('ListWeakestWordCtrl', ['$scope', '$location', 'WeakestWordServices', ListWeakestWordCtrl])
-    .controller('WeakestWordDetailCtrl', ['$scope', '$location', 'WeakestWordServices', WeakestWordDetailCtrl])
     .factory('WeakestWordServices', ['$http', '$q', '$localStorage', 'API', WeakestWordServices]);
 }(window.angular));
