fc57469528a3c1ba769d81c5002385943e3e1cb8
Nguyễn Phan Hải <hainp@topica.edu.vn>
2015-04-03 14:23:24 +0700

    fix(forum): sticky_posts

--- app/forum/controllers.js
+++ app/forum/controllers.js
@@ -11,6 +11,12 @@
           return post;
         });
         output.current_page = output.next_page > 0 ? output.next_page - 1 : output.total_page;
+        if (output.sticky_posts) {
+          output.sticky_posts = output.sticky_posts.map(function (post) {
+            post.created_time = Math.round((new Date('' + post.created_at)).getTime() / 1000);
+            return post;
+          });
+        }
       }
       return output;
     }
@@ -84,7 +90,6 @@
         post.is_followed = false;
       });
     };
-
 
     $scope.setPage = function (page) {
       var search = {
@@ -300,7 +305,7 @@
      * Vote up cho 1 comment
      * @comment: comment duoc vote
      */
-     $scope.voteUpComment = function (comment) {
+    $scope.voteUpComment = function (comment) {
       if (comment.is_vote_up) {
         comment.up_vote_count = comment.up_vote_count - 1;
       } else {
@@ -337,18 +342,19 @@
     };
 
     $scope.reply = function (comment) {
-      ForumServices.listReply({id: comment._id}).success(function (data) {
+      ForumServices.listReply({
+        id: comment._id
+      }).success(function (data) {
         $scope.replies = data.comments;
       });
     };
   }
 
-
   angular.module('forum.controllers', ['forum.services'])
-  .controller('ListPostCtrl', ['$scope', '$location', 'ForumServices', 'allPosts', 'subscribers', 'followingPosts',
-    'searchPosts',
-    ListPostCtrl
+    .controller('ListPostCtrl', ['$scope', '$location', 'ForumServices', 'allPosts', 'subscribers', 'followingPosts',
+      'searchPosts',
+      ListPostCtrl
     ])
-  .controller('CreatePostCtrl', ['$scope', 'ForumServices', '$location', 'subscribers', CreatePostCtrl])
-  .controller('PostDetailCtrl', ['$scope', 'ForumServices', '$location', 'Post', 'subscribers', PostDetailCtrl]);
+    .controller('CreatePostCtrl', ['$scope', 'ForumServices', '$location', 'subscribers', CreatePostCtrl])
+    .controller('PostDetailCtrl', ['$scope', 'ForumServices', '$location', 'Post', 'subscribers', PostDetailCtrl]);
 }(window.angular));
