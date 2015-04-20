412eee59c0481fff89ba39ee65a2b3b92b018487
Dao Thanh Luan <luandt@topica.edu.vn>
2015-04-20 09:24:26 +0700

    fix job-list

--- app/services.js
+++ app/services.js
@@ -212,10 +212,7 @@
 
   angular.module('app.services', [])
     .constant('APP_VERSION', '1.0.3')
-    .factory('API', ['$location', function ($location) {
-      var host = $location.host();
-      return host.match(/(^memo.|.edu.vn$)/g) ? '//services.memo.edu.vn/v2/api' : '//staging.memo.edu.vn/v2/api'
-    }])
+    .constant('API', '//services.memo.edu.vn/v2/api')
     .constant('angularMomentConfig', {
       preprocess: 'unix',
       timezone: 'Asia/Hanoi'
