53693800efa54ede6d8b4e5bedb7ca716d6c0660
Dao Thanh Luan <luandt@topica.edu.vn>
2015-06-08 10:48:56 +0700

    fix(dicussion): fix bug functional turn I/O dicussion

--- app/components/player/player.js
+++ app/components/player/player.js
@@ -2,6 +2,23 @@
   'use strict';
 
   function PlayerCtrl($scope) {
+    $scope.sharedSettings = {
+      functionaly: {
+        should_weakest_word: true,
+        should_forum:  true,
+        should_jobs: true,
+        should_profile: true
+      }
+    };
+
+    AppSetting.getSharedSettings().then(function () {
+      $scope.sharedSettings = AppSetting.shared_settings;
+      $rootScope.$broadcast('event-sharedSettingsLoaded');
+    });
+
+    $scope.$on('event-sharedSettingsLoaded', function () {
+      $scope.sharedSettings = AppSetting.sharedSettings;
+    });
   }
 
   angular.module('player', []);
