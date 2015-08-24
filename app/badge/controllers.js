03e20391e82e9d5bb9b4572b2e4e98a48ae944b9
Jang Eddy <Jang@Jangs-MacBook-Air.local>
2015-08-24 09:42:07 +0700

    change link share badge

--- app/badge/controllers.js
+++ app/badge/controllers.js
@@ -26,7 +26,7 @@
         var data = {
           caption: 'http://memo.edu.vn', //name
           description: 'TOPICA Memo là ứng dụng học tiếng Anh hoàn toàn MIỄN PHÍ. Các bài học được thiết kế dưới dạng trò chơi khiến cho việc học tiếng Anh chưa bao giờ DỄ DÀNG và THÚ VỊ đến thế. Tham gia học với kiến '+ $localStorage.auth.user.username +' ngay để tiến bộ từng ngày!',
-          link: "http://memo.edu.vn",
+          link: "http://memo.edu.vn/?code_chanel=ShareFB&id_landingpage=2&id_campaign=1&id=199",
           name: 'Chúc mừng '+ $localStorage.auth.user.username +' đã dành được huy hiệu ' + badge.name,
           picture: badge.share_image,
           method: 'feed'
